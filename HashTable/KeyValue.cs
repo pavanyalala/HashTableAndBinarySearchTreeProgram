ce187de3743e10a94056fa3126af08b33e00a409
pavanyalala <pavanyalala4508@gmail.com>
2021-08-10 14:03:38 +0530

    [pavan]:Hash map program implemented

---
+++ HashTable/KeyValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+    
+}
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -7,6 +7,21 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Map");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash4 = hash.Get("4");
+            Console.WriteLine("fourth index value : " + hash4);
+            hash.Remove("1");
+            string hash1 = hash.Get("1");
+            Console.WriteLine("First index value:" + hash1);
         }
     }
+    
 }
